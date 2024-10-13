e7862684571c7dc84d1228cf4767655d46384a5d
Rudresh07 <97966593+Rudresh07@users.noreply.github.com>
2024-10-13 22:25:40 +0530

    initial commit

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_rudy_stockmarketapp_StockApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.rudy.stockmarketapp.StockApplication"
-)
-public final class _com_rudy_stockmarketapp_StockApplication {
-}
